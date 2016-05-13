8f4e6d7542c8f883a4bb75312bf111b298e3a871
Riku Rouvila <riku.rouvila@gmail.com>
2016-05-14 00:14:06 +0300

    use textarea instead of text input

--- src/components/ThoughtInput.elm
+++ src/components/ThoughtInput.elm
@@ -2,7 +2,7 @@ module Components.ThoughtInput where
 
 import Html exposing (..)
 import Html.Attributes exposing (type', class, value, autofocus)
-import Html.Events exposing (onClick, on, targetValue, keyCode)
+import Html.Events exposing (onClick, on, targetValue, keyCode, onWithOptions)
 
 import Signal exposing (Signal, Address)
 import Json.Decode as Json
@@ -35,23 +35,34 @@ update action model =
 view : Context -> Model -> Html
 view context model =
   div []
-    [ input
+    [ textarea
         [ class "form-control"
         , type' "text"
         , value model.text
         , autofocus True
         , on "input" targetValue (Signal.message context.actions << UpdateField)
         , onEnter context.submit Submit
-        ]
-        []
-  ]
+        ] []
+    ]
 
 isEnter : Int -> Result String ()
 isEnter code =
   if code == 13 then Ok () else Err "not the right key code"
 
-onEnter : Address a -> a -> Attribute
+noShiftKey : Bool -> Result String ()
+noShiftKey bool =
+  if not bool then Ok () else Err "shift is pressed"
+
+onEnter : Address Action -> Action -> Attribute
 onEnter address value =
-    on "keydown"
-      (Json.customDecoder keyCode isEnter)
-      (\_ -> Signal.message address value)
+    let
+      options = { stopPropagation = True
+                , preventDefault = True
+                }
+      decoder = (Json.customDecoder keyCode isEnter) `Json.andThen`
+        always (Json.customDecoder (Json.at ["shiftKey"] Json.bool) noShiftKey)
+    in
+      onWithOptions "keydown"
+        options
+        decoder
+        (\_ -> Signal.message address value)
